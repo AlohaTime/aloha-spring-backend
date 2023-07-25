42134504d18189207e41b5780454c1e2b69b3b9e
soyeon207 <psy010207@naver.com>
2023-07-26 01:33:49 +0900

    🎉 프로젝트 init

---
+++ src/test/java/com/inha/plus/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inha.plus.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
