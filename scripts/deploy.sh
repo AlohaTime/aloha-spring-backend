749d2eb582633dafa6df258de0f474be6001f130
mallin <39696812+soyeon207@users.noreply.github.com>
2023-11-09 02:40:29 +0900

    👷 살짝 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,14 +9,15 @@ APPLICATION_LOG_PATH="/home/ubuntu/$PROJECT_NAME/log/application.log"
 BUILD_JAR=$(ls $JAR_PATH)
 JAR_NAME=$(basename $BUILD_JAR)
 
-echo "===== 배포 시작 : $(date +%c) =====" >> $DEPLOY_LOG_PATH
 
-echo "> build 파일명: $JAR_NAME" >> $DEPLOY_LOG_PATH
-echo "> build 파일 복사" >> $DEPLOY_LOG_PATH
-cp $BUILD_JAR $DEPLOY_PATH
+# echo "===== 배포 시작 : $(date +%c) =====" >> $DEPLOY_LOG_PATH
 
-echo "> 현재 동작중인 어플리케이션 pid 체크" >> $DEPLOY_LOG_PATH
-CURRENT_PID=$(pgrep -f $JAR_NAME)
+# echo "> build 파일명: $JAR_NAME" >> $DEPLOY_LOG_PATH
+# echo "> build 파일 복사" >> $DEPLOY_LOG_PATH
+# cp $BUILD_JAR $DEPLOY_PATH
+
+# echo "> 현재 동작중인 어플리케이션 pid 체크" >> $DEPLOY_LOG_PATH
+# CURRENT_PID=$(pgrep -f $JAR_NAME)
 
 if [ -z $CURRENT_PID ]
 then
@@ -28,12 +29,17 @@ else
   kill -9 $CURRENT_PID
 fi
 
+cd $DEPLOY_PATH
+
+./gradlew build
+./gradlew build
+
 DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
-echo "> DEPLOY_JAR 배포" >> $DEPLOY_JAR
+# echo "> DEPLOY_JAR 배포" >> $DEPLOY_JAR
 mkdir $DEPLOY_LOG_DIR_PATH
 
-nohup java -jar -Dspring.profiles.active=local $DEPLOY_JAR --server.port=8080 >> $APPLICATION_LOG_PATH 2> $DEPLOY_ERR_LOG_PATH &
+nohup java -jar -Dspring.profiles.active=local $JAR_PATH --server.port=8080 >> $APPLICATION_LOG_PATH 2> $DEPLOY_ERR_LOG_PATH &
 
 sleep 3
 
-echo "> 배포 종료 : $(date +%c)" >> $DEPLOY_LOG_PATH
+# echo "> 배포 종료 : $(date +%c)" >> $DEPLOY_LOG_PATH
