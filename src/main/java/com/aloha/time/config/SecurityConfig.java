4d944c12bb8396b78064d0ef89941aa9fb3d1280
soyeon207 <psy010207@naver.com>
2023-11-13 23:54:11 +0900

    security 원복

--- src/main/java/com/aloha/time/config/SecurityConfig.java
+++ src/main/java/com/aloha/time/config/SecurityConfig.java
@@ -8,14 +8,14 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class SecurityConfig {
 
-     @Bean
-     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-         return http
-                 .requiresChannel(channel ->
-                         channel.anyRequest().requiresSecure())
-                 .authorizeRequests(authorize ->
-                         authorize.anyRequest().permitAll())
-                 .build();
-     }
+//     @Bean
+//     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//         return http
+//                 .requiresChannel(channel ->
+//                         channel.anyRequest().requiresSecure())
+//                 .authorizeRequests(authorize ->
+//                         authorize.anyRequest().permitAll())
+//                 .build();
+//     }
 
 }
