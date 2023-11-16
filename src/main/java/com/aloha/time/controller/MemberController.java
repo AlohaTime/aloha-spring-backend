0999380bf7bd61e3eda40fee998fba8cad09b7a0
soyeon207 <psy010207@naver.com>
2023-11-16 23:38:36 +0900

    👷 배포 테스트

--- src/main/java/com/aloha/time/controller/MemberController.java
+++ src/main/java/com/aloha/time/controller/MemberController.java
@@ -13,7 +13,7 @@ public class MemberController {
 
     @GetMapping("/test")
     public String test() {
-        return "배포테스트";
+        return "배포테스트2";
     }
 
     // 로그인이나 출석정보AttendanceGetRequest
